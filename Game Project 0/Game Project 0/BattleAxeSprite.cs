061ea75f61f50e724c21d0bf48f2d164adfcdf36
Mason Wittman <mawittma@ksu.edu>
2022-01-28 20:08:49 -0600

    Final commit

---
+++ Game Project 0/Game Project 0/BattleAxeSprite.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Game_Project_0
+{
+    /// <summary>
+    /// Class to represent the Battle Axe Sprite
+    /// </summary>
+    public class BattleAxeSprite
+    {
+        private Texture2D texture;
+
+        private double animationTimer;
+
+        private short animationFrame = 0;
+        /// <summary>
+        /// Vector2 to represent the position of the battle axe
+        /// </summary>
+        public Vector2 Position; 
+        /// <summary>
+        /// loads the content of the battleaxe-sheet
+        /// </summary>
+        /// <param name="content">Content manager for project</param>
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("battleaxe-sheet");
+        }
+
+        
+        /// <summary>
+        /// Draws the animated axe sprite
+        /// </summary>
+        /// <param name="gameTime">the game time</param>
+        /// <param name="spriteBatch">The spritebatch to draw with</param>
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //update animation timer
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            //update animation frame
+            if(animationTimer > .3)
+            {
+                animationFrame++;
+                if (animationFrame >7) animationFrame = 0;
+                animationTimer -= .3;
+            }
+
+            // draw the sprite
+            var source = new Rectangle(animationFrame*16, 0, 16, 16);
+            spriteBatch.Draw(texture, Position, source, Color.White, 0f, new Vector2(0,0), 2f, new SpriteEffects(), 0);
+
+        }
+    }
+}
--- Game Project 0/Game Project 0/TitleScreen.cs
+++ Game Project 0/Game Project 0/TitleScreen.cs
@@ -10,47 +10,83 @@ namespace Game_Project_0
         private SpriteBatch _spriteBatch;
         private SpriteFont earthboundBig;
         private SpriteFont earthboundSmall;
+        private BattleAxeSprite[] axes;
+        private Texture2D atlas;
 
+        /// <summary>
+        /// class initilaizer 
+        /// </summary>
         public TitleScreen()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+        /// <summary>
+        /// initialize battleaxe sprites in an array
+        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //
+            axes = new BattleAxeSprite[]
+            {
+                new BattleAxeSprite() {Position = new Vector2(100,50)},
+                new BattleAxeSprite() {Position = new Vector2(600,400)},
+                new BattleAxeSprite() {Position = new Vector2(100,400)},
+                new BattleAxeSprite() {Position = new Vector2(600,50)},
+            };
             base.Initialize();
         }
-
+        /// <summary>
+        /// Load contents of class
+        /// </summary>
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             earthboundBig = Content.Load<SpriteFont>("EarthboundBig");
             earthboundSmall = Content.Load<SpriteFont>("EarthboundSmall");
-
+            foreach (var axe in axes) axe.LoadContent(Content);
+            atlas = Content.Load<Texture2D>("colored_packed");
             // TODO: use this.Content to load your game content here
         }
-
+        /// <summary>
+        /// updates the game
+        /// </summary>
+        /// <param name="gameTime">gameTime variable</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-
+             
             base.Update(gameTime);
         }
-
+        /// <summary>
+        /// Drawing the spritebatch 
+        /// </summary>
+        /// <param name="gameTime">gameTime variable</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Orange);
+            GraphicsDevice.Clear(Color.Green);
 
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(earthboundBig, "Rocket League", new Vector2(200, 150), Color.Black);
-            _spriteBatch.DrawString(earthboundSmall, "Hit 'ESC' on keyboard or 'back' on controller to exit", new Vector2(15,400), Color.Black);
+            _spriteBatch.DrawString(earthboundBig, "Casino", new Vector2(275, 150), Color.Red);
+            _spriteBatch.DrawString(earthboundBig, "Royale", new Vector2(275, 220), Color.Black);
+
+            _spriteBatch.DrawString(earthboundSmall, "Hit 'ESC' on keyboard or 'back' on controller to exit", new Vector2(15,450), Color.Black);
+            foreach (var axe in axes) axe.Draw(gameTime, _spriteBatch);
+
+            _spriteBatch.Draw(atlas, new Vector2(325, 50),new Rectangle(20*16, 16*16, 16, 16), Color.Red,.25f, new Vector2(0,0), 3f, new SpriteEffects(),0 );
+            _spriteBatch.Draw(atlas, new Vector2(330, 90), new Rectangle(32 * 16, 8 * 16, 16, 16), Color.Black, -1f, new Vector2(0, 0), 3f, new SpriteEffects(), 1);
+            _spriteBatch.Draw(atlas, new Vector2(325, 350), new Rectangle(20 * 16, 19 * 16, 16, 16), Color.Black, .25f, new Vector2(0, 0), 3f, new SpriteEffects(), 0);
+            _spriteBatch.Draw(atlas, new Vector2(330, 390), new Rectangle(33 * 16, 2 * 16, 16, 16), Color.Red, -1f, new Vector2(0, 0), 3f, new SpriteEffects(), 1);
+
+            _spriteBatch.Draw(atlas, new Vector2(100, 200), new Rectangle(20 * 16, 17 * 16, 16, 16), Color.Red, .25f, new Vector2(0, 0), 3f, new SpriteEffects(), 0);
+            _spriteBatch.Draw(atlas, new Vector2(105, 240), new Rectangle(37 * 16, 7 * 16, 16, 16), Color.Black, -1f, new Vector2(0, 0), 3f, new SpriteEffects(), 1);
+            _spriteBatch.Draw(atlas, new Vector2(600, 200), new Rectangle(20 * 16, 18 * 16, 16, 16), Color.Black, .25f, new Vector2(0, 0), 3f, new SpriteEffects(), 0);
+            _spriteBatch.Draw(atlas, new Vector2(605, 240), new Rectangle(42 * 16, 8 * 16, 16, 16), Color.Red, -1f, new Vector2(0, 0), 3f, new SpriteEffects(), 1);
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
